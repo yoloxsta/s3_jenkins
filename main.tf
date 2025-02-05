1ca42b6c0fa8e743553557fa70d552b88f4b10c7
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-02-05 10:01:16 +0630

    test

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "yoloxstaaaay"
+
+  tags = {
+    Name        = "MyS3Bucket"
+    Environment = "Dev"
+  }
+}

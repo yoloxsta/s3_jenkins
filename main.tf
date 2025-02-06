5dec85fa228a2e59c012d47f314bf62834a05cc2
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-02-06 14:15:20 +0630

    update:jenkisnfile

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-# provider "aws" {
-#   region = "us-east-1"  # Replace with your desired region
-# }
+ provider "aws" {
+   region = "us-east-1"  # Replace with your desired region
+ }
 
 # resource "aws_s3_bucket" "example" {
 #   bucket = var.bucket_name

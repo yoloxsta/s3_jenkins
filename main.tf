63603eb9930e93aefd1326493e8c18b72146221d
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-02-06 13:13:20 +0630

    update:jenkisnfile

--- main.tf
+++ main.tf
@@ -4,10 +4,13 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
-  acl    = "private"
 
   tags = {
     Name        = "MyBucket"
     Environment = "Dev"
   }
+}
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.bucket
+  acl    = "private"
 }
